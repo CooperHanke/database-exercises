089b0eeba37f60d9440c0bcd19dcf509f560189e
coopmostcurious <hanke31314@gmail.com>
2018-03-15 14:59:05 -0500

    finished with the exercise with Basic Statements

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
 SELECT 'Albums released after 1990' AS 'Info';
-SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 SELECT 'Albums with the genre disco' AS 'Info';
-SELECT * FROM albums WHERE genre = 'Disco';
+DELETE FROM albums WHERE genre = 'Disco';
 SELECT 'Albums by Metallica' AS 'Info';
-SELECT * FROM albums WHERE artist = 'Metallica';
+DELETE FROM albums WHERE artist = 'Metallica';

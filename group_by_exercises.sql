0ef35237ba4df2c04a4741870ea04db9449a3d3f
coopmostcurious <hanke31314@gmail.com>
2018-03-18 09:13:41 -0500

    finished with exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,6 @@
+USE titles;
+SELECT DISTINCT(title) from titles;
+USE employees;
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e' GROUP BY first_name, last_name;
+SELECT DISTINCT last_name, count(*) from employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+SELECT gender, count(*) FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender;

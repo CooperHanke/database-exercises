5fd796124ad66642ec2f1bf8e07c6e3d72d141af
coopmostcurious <hanke31314@gmail.com>
2018-03-19 11:10:21 -0500

    finished exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -6,3 +6,18 @@ WHERE hire_date IN (
   FROM employees
   WHERE emp_no = 101010
 );
+SELECT title
+FROM titles
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE first_name = 'Aamod'
+);
+SELECT concat(first_name, ' ', last_name) AS current_female_managers
+FROM employees
+WHERE gender = 'F' AND emp_no IN (
+  SELECT emp_no
+  FROM titles
+  WHERE title = 'Manager'
+  AND to_date > now()
+);

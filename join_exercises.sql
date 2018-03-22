6126affca8168aadd7ccea3245dc73e5dcf2af1e
coopmostcurious <hanke31314@gmail.com>
2018-03-22 09:51:35 -0500

    completed bonus on join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -37,7 +37,7 @@ GROUP BY t.title;
 
 -- get department name | full name of manager | salary of manager in ?
 USE employees;
-SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', concat('$ ',s.salary) AS 'Salary'
 FROM departments AS d
   JOIN dept_manager AS dm
     ON d.dept_no = dm.dept_no
@@ -47,3 +47,15 @@ FROM departments AS d
     ON e.emp_no = s.emp_no
 WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 ORDER BY d.dept_name;
+
+-- get employee's full name | that employee's dept | that dept's manager
+SELECT concat(e.first_name, ' ',e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name',
+  concat(e2.first_name, ' ', e2.last_name) AS 'Manager Name'
+FROM employees AS e
+  JOIN dept_emp AS emp
+    ON e.emp_no = emp.emp_no
+  JOIN departments d ON emp.dept_no = d.dept_no
+  JOIN dept_manager manager ON d.dept_no = manager.dept_no
+  JOIN employees e2 ON manager.emp_no = e2.emp_no
+WHERE manager.to_date > curdate() AND emp.to_date > curdate();
+
